0ea4f2f913d311a2c6a27441c5239d6db5235270
Koushik Saha <koushik.saha666@gmail.com>
2025-03-20 16:09:45 -0700

    Send Email When a Donation is Completed

--- animals/utils.py
+++ animals/utils.py
@@ -1,6 +1,16 @@
 from django.core.mail import send_mail
 from django.conf import settings
 
+
+def send_donation_email(user_email, amount):
+    send_mail(
+        "Thank You for Your Donation!",
+        f"Dear supporter,\n\nThank you for your generous donation of ${amount}. Your support helps us rescue and care for more animals.\n\nBest regards,\nAnimal Shelter Team",
+        "your-email@gmail.com",
+        [user_email],
+        fail_silently=False,
+    )
+
 def send_email(to_email, subject, message):
     send_mail(
         subject,
