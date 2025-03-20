2f6bff78787567254e15798a2f08f560eb8276e8
Koushik Saha <koushik.saha666@gmail.com>
2025-03-20 16:11:25 -0700

    Implement User Profiles - Support User Profiles

--- animals/models.py
+++ animals/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    adopted_animals = models.ManyToManyField("Animal", blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+
 
 class Donation(models.Model):
     STATUS_PENDING = "Pending"
