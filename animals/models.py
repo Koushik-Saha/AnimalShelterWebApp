fc9f9fa2abe509b1db47b707317f27747ba8d308
Koushik Saha <koushik.saha666@gmail.com>
2025-03-20 22:34:28 -0700

    Create a Profile Model - Image Add

--- animals/models.py
+++ animals/models.py
@@ -6,6 +6,7 @@ class Profile(models.Model):
     phone = models.CharField(max_length=15, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
     adopted_animals = models.ManyToManyField("Animal", blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
