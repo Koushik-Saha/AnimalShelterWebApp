9bf7860652b9d56868bd5252e2d778fce8547be8
Koushik Saha <koushik.saha666@gmail.com>
2025-03-22 13:18:08 -0700

    Add Redis Cache in settings.py

--- animal_shelter/settings.py
+++ animal_shelter/settings.py
@@ -151,6 +151,16 @@ LOGGING = {
     },
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",  # Database 1
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
 # Load environment variables from .env file
 load_dotenv()
 
