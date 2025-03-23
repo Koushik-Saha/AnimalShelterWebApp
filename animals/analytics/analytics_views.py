978b8b1f76b94312f717a1a238f19c9829158ca6
Koushik Saha <koushik.saha666@gmail.com>
2025-03-23 00:46:19 -0700

    Create a donation trend analytics API

--- animals/analytics/analytics_views.py
+++ animals/analytics/analytics_views.py
@@ -1,9 +1,9 @@
-from django.db.models import Count
+from django.db.models import Count, Sum
 from django.db.models.functions import TruncMonth
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAdminUser
-from animals.models import AdoptionRequest
+from animals.models import AdoptionRequest, Donation
 
 
 class AdoptionSuccessAnalyticsView(APIView):
@@ -19,3 +19,17 @@ class AdoptionSuccessAnalyticsView(APIView):
             .order_by("month")
         )
         return Response(data)
+
+class DonationTrendAnalyticsView(APIView):
+    permission_classes = [IsAdminUser]
+
+    def get(self, request):
+        donations = (
+            Donation.objects
+            .filter(status="Completed")
+            .annotate(month=TruncMonth("created_at"))
+            .values("month")
+            .annotate(total_donated=Sum("amount"))
+            .order_by("month")
+        )
+        return Response(donations)
