2fdcaeb5c30b65432b11931969983266e54b9487
Koushik Saha <koushik.saha666@gmail.com>
2025-03-24 20:47:05 -0700

    Add URL routes for CRUD operations on MedicalRecord

--- animals/medicalcare/urls.py
+++ animals/medicalcare/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import MedicalRecordCreateView
+from .views import MedicalRecordCreateView, MedicalRecordListView, MedicalRecordDetailView, MedicalRecordUpdateView, \
+    MedicalRecordDeleteView
 
 urlpatterns = [
-    path('create/', MedicalRecordCreateView.as_view(), name='create-medical-record'),
+    path('create/', MedicalRecordCreateView.as_view(), name='medical-create'),
+    path('list/', MedicalRecordListView.as_view(), name='medical-list'),
+    path('<int:pk>/', MedicalRecordDetailView.as_view(), name='medical-detail'),
+    path('<int:pk>/update/', MedicalRecordUpdateView.as_view(), name='medical-update'),
+    path('<int:pk>/delete/', MedicalRecordDeleteView.as_view(), name='medical-delete'),
 ]
