d51187390cf6a08e2cb61f11f39fa2bea72be2a9
Koushik Saha <koushik.saha666@gmail.com>
2025-03-26 02:17:50 -0700

    Add URL for adoption application status update

--- animals/adoptionprocessmanagement/urls.py
+++ animals/adoptionprocessmanagement/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import AdoptionApplicationCreateView, AdoptionApplicationListView, AdoptionApplicationDetailView
+from .views import AdoptionApplicationCreateView, AdoptionApplicationListView, AdoptionApplicationDetailView, \
+    AdoptionApplicationUpdateView
 
 urlpatterns = [
     path("adoption-applications/create/", AdoptionApplicationCreateView.as_view(), name="create-adoption-application"),
 
     path("adoption-applications/", AdoptionApplicationListView.as_view(), name="list-adoption-applications"),
     path("adoption-applications/<int:pk>/", AdoptionApplicationDetailView.as_view(), name="detail-adoption-application"),
+
+    path("adoption-applications/<int:pk>/update/", AdoptionApplicationUpdateView.as_view(),
+         name="update-adoption-application"),
+
 ]
