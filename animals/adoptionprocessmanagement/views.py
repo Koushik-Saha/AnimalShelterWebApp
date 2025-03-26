efb6d81eb2ada7518472ae71d3590948e2772608
Koushik Saha <koushik.saha666@gmail.com>
2025-03-26 02:14:03 -0700

    Add list and detail view for adoption applications with admin/staff permissions

--- animals/adoptionprocessmanagement/views.py
+++ animals/adoptionprocessmanagement/views.py
@@ -1,8 +1,23 @@
 from rest_framework import generics, permissions
+from rest_framework.permissions import IsAuthenticated
+
 from .serializers import AdoptionApplicationSerializer
 from .models import AdoptionApplication
+from ..permissions import IsStaffOrAdmin
+
 
 class AdoptionApplicationCreateView(generics.CreateAPIView):
     queryset = AdoptionApplication.objects.all().order_by('-id')
     serializer_class = AdoptionApplicationSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+class AdoptionApplicationListView(generics.ListAPIView):
+    queryset = AdoptionApplication.objects.all().order_by('-created_at')
+    serializer_class = AdoptionApplicationSerializer
+    permission_classes = [IsAuthenticated, IsStaffOrAdmin]
+
+
+class AdoptionApplicationDetailView(generics.RetrieveAPIView):
+    queryset = AdoptionApplication.objects.all()
+    serializer_class = AdoptionApplicationSerializer
+    permission_classes = [IsAuthenticated, IsStaffOrAdmin]
