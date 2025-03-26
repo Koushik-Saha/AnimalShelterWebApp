fb9ae943eafb1db2222a72fa3679b6d6823429f4
Koushik Saha <koushik.saha666@gmail.com>
2025-03-26 00:59:09 -0700

    Create TrainingNoteCreateView, TrainingNoteListView, TrainingNoteDetailView with permission checks

--- animals/behavior/views.py
+++ animals/behavior/views.py
@@ -1,7 +1,10 @@
 from rest_framework import generics, permissions
-from .models import BehaviorAssessment, EnrichmentActivity
-from .serializers import BehaviorAssessmentSerializer, EnrichmentActivitySerializer
-from .permissions import CanManageBehaviorAssessment
+from rest_framework.permissions import IsAuthenticated
+
+from .models import BehaviorAssessment, EnrichmentActivity, TrainingNote
+from .serializers import BehaviorAssessmentSerializer, EnrichmentActivitySerializer, TrainingNoteSerializer
+from .permissions import CanManageBehaviorAssessment, IsStaffOrReadOnly
+
 
 class CreateBehaviorAssessmentView(generics.CreateAPIView):
     queryset = BehaviorAssessment.objects.all()
@@ -38,3 +41,21 @@ class EnrichmentActivityListView(generics.ListAPIView):
 
     def get_queryset(self):
         return EnrichmentActivity.objects.all()
+
+class TrainingNoteCreateView(generics.CreateAPIView):
+    queryset = TrainingNote.objects.all()
+    serializer_class = TrainingNoteSerializer
+    permission_classes = [IsAuthenticated, IsStaffOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
+class TrainingNoteListView(generics.ListAPIView):
+    queryset = TrainingNote.objects.all()
+    serializer_class = TrainingNoteSerializer
+    permission_classes = [IsAuthenticated]
+
+class TrainingNoteDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = TrainingNote.objects.all()
+    serializer_class = TrainingNoteSerializer
+    permission_classes = [IsAuthenticated, IsStaffOrReadOnly]
