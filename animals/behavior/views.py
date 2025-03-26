ea3463fbea5e65d860736fc965760f23727576f2
Koushik Saha <koushik.saha666@gmail.com>
2025-03-26 00:34:39 -0700

    Implement create and list views for enrichment activity tracking

--- animals/behavior/views.py
+++ animals/behavior/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics, permissions
-from .models import BehaviorAssessment
-from .serializers import BehaviorAssessmentSerializer
+from .models import BehaviorAssessment, EnrichmentActivity
+from .serializers import BehaviorAssessmentSerializer, EnrichmentActivitySerializer
 from .permissions import CanManageBehaviorAssessment
 
 class CreateBehaviorAssessmentView(generics.CreateAPIView):
@@ -22,3 +22,19 @@ class BehaviorAssessmentDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = BehaviorAssessment.objects.all()
     serializer_class = BehaviorAssessmentSerializer
     permission_classes = [permissions.IsAuthenticated, CanManageBehaviorAssessment]
+
+
+class EnrichmentActivityCreateView(generics.CreateAPIView):
+    serializer_class = EnrichmentActivitySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
+
+class EnrichmentActivityListView(generics.ListAPIView):
+    serializer_class = EnrichmentActivitySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return EnrichmentActivity.objects.all()
