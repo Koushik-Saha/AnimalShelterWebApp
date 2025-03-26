c4c00d8ef5a44feeba695bb0e4e3c1876e1b04e3
Koushik Saha <koushik.saha666@gmail.com>
2025-03-26 00:32:15 -0700

    Create EnrichmentActivitySerializer for enrichment tracking

--- animals/behavior/serializers.py
+++ animals/behavior/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
-from .models import BehaviorAssessment
+from .models import BehaviorAssessment, EnrichmentActivity
+
 
 class BehaviorAssessmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = BehaviorAssessment
         fields = '__all__'
         read_only_fields = ['observer', 'created_at']
+
+class EnrichmentActivitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EnrichmentActivity
+        fields = '__all__'
+        read_only_fields = ['id', 'created_by', 'created_at']
