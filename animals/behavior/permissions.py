3729e1a36473e596360c8acf49af5e48a73b02e9
Koushik Saha <koushik.saha666@gmail.com>
2025-03-26 00:58:23 -0700

    Create IsStaffOrReadOnly permission for TrainingNote views

--- animals/behavior/permissions.py
+++ animals/behavior/permissions.py
@@ -1,7 +1,14 @@
 from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
 class CanManageBehaviorAssessment(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and (
             request.user.role in ['staff', 'admin']
         )
+
+class IsStaffOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user and request.user.is_staff
