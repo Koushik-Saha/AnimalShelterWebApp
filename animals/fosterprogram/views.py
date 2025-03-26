6571e6231d06b60f3679363615cd943d384df3c8
Koushik Saha <koushik.saha666@gmail.com>
2025-03-26 12:44:22 -0700

    Add Foster Application CRUD

--- animals/fosterprogram/views.py
+++ animals/fosterprogram/views.py
@@ -1,12 +1,33 @@
-from rest_framework import generics, permissions
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
 from .models import FosterApplication
 from .serializers import FosterApplicationSerializer
-from .permissions import IsUserOrAdmin
 
 class FosterApplicationCreateView(generics.CreateAPIView):
-    queryset = FosterApplication.objects.all()
+    queryset = FosterApplication.objects.all().order_by('-id')
     serializer_class = FosterApplicationSerializer
-    permission_classes = [permissions.IsAuthenticated, IsUserOrAdmin]
+    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+class FosterApplicationListView(generics.ListAPIView):
+    queryset = FosterApplication.objects.all().order_by('-id')
+    serializer_class = FosterApplicationSerializer
+    permission_classes = [IsAdminUser]
+
+class FosterApplicationDetailView(generics.RetrieveAPIView):
+    queryset = FosterApplication.objects.all().order_by('-id')
+    serializer_class = FosterApplicationSerializer
+    permission_classes = [IsAdminUser]
+
+class FosterApplicationUpdateView(generics.UpdateAPIView):
+    queryset = FosterApplication.objects.all().order_by('-id')
+    serializer_class = FosterApplicationSerializer
+    permission_classes = [IsAdminUser]
+
+class FosterApplicationDeleteView(generics.DestroyAPIView):
+    queryset = FosterApplication.objects.all().order_by('-id')
+    serializer_class = FosterApplicationSerializer
+    permission_classes = [IsAdminUser]
