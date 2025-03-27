662c2c66a3636b618c7d91a0b0017ac93be9fa60
Koushik Saha <koushik.saha666@gmail.com>
2025-03-27 12:51:33 -0700

    register LostPetReport model in Django admin

--- animals/admin.py
+++ animals/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+
+from animals.lostpetreporting.models import LostPetReport
 from animals.models import Animal, AdoptionRequest, Donation
 
 @admin.register(Animal)
@@ -14,3 +16,5 @@ class DonationAdmin(admin.ModelAdmin):
     list_display = ('user', 'amount', 'status', 'created_at')
     search_fields = ('user__username', 'transaction_id')
     list_filter = ('status',)
+
+admin.site.register(LostPetReport)
