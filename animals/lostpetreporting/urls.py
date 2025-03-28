25b478d0116d3775b8a34ce10be6102bdac300f2
Koushik Saha <koushik.saha666@gmail.com>
2025-03-28 10:40:07 -0700

    add route to match lost pets with found animals

--- animals/lostpetreporting/urls.py
+++ animals/lostpetreporting/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import LostPetReportView, FoundAnimalView
+from .views import LostPetReportView, FoundAnimalView, MatchLostPetWithFoundAnimalsView
 
 urlpatterns = [
     path('lost-pets/', LostPetReportView.as_view(), name='lost-pet-list-create'),
     path('lost-pets/<int:pk>/', LostPetReportView.as_view(), name='lost-pet-detail-update-delete'),
 
     path('found-animals/', FoundAnimalView.as_view(), name='found-animal-crud'),
+
+    path("lost-pets/<int:lost_pet_id>/match/", MatchLostPetWithFoundAnimalsView.as_view(),
+         name="match-lost-found"),
 
 ]
