6c7713b4e4c3771bc44a247064d783e4988b45eb
Koushik Saha <koushik.saha666@gmail.com>
2025-03-29 02:26:59 -0700

    Update serializers

--- animals/inventory/serializers.py
+++ animals/inventory/serializers.py
@@ -15,7 +15,7 @@ class LowStockAlertSerializer(serializers.ModelSerializer):
 class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
-        fields = '__all__'
+        exclude = ['order']
 
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True)
