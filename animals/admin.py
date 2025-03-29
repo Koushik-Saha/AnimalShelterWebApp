fa9ee29b4f0d255ba16d3ea51af1ef5b7d3a7061
Koushik Saha <koushik.saha666@gmail.com>
2025-03-29 03:19:55 -0700

    Implement Custom User Model with Role and Contact Details for Bunny Shelter Users

--- animals/admin.py
+++ animals/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
 from animals.lostpetreporting.models import LostPetReport
-from animals.models import Animal, AdoptionRequest, Donation
+from animals.models import Animal, AdoptionRequest, Donation, CustomUser
+
 
 @admin.register(Animal)
 class AnimalAdmin(admin.ModelAdmin):
@@ -16,5 +17,14 @@ class DonationAdmin(admin.ModelAdmin):
     list_display = ('user', 'amount', 'status', 'created_at')
     search_fields = ('user__username', 'transaction_id')
     list_filter = ('status',)
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'role', 'is_active']
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('role', 'full_name', 'phone', 'address')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
 
 admin.site.register(LostPetReport)
